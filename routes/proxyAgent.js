5d9b89eb425821b07c9d6619e73d731fbf21aa6a
Edward <cololacalempira5@gmail.com>
2026-08-26 21:40:13 -0600

    Eliminar proxyAgent.js

--- routes/proxyAgent.js
+++
@@ -1,5 +0,0 @@
-import { HttpsProxyAgent } from 'https-proxy-agent';
-
-const PROXY_URL = "http://qmefplrt:p6d27d6c5uku@31.59.20.176:6754";
-
-export const proxyAgent = new HttpsProxyAgent(PROXY_URL);
